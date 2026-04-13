39c9fe95bb10e5cb8bdd2a630c58bb2de5eba12b
Ivan_zolo 2004 <muhanov.danil175@gmail.com>
2026-04-13 14:44:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -97,6 +97,6 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("http://localhost:3000");
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Server started");
 });
